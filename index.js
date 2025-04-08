b39294a032b05afb9dfc559470669a0660e47a44
silentm4gician <leandrogonzalezmat@gmail.com>
2025-04-08 01:49:53 -0300

    index info

--- index.js
+++ index.js
@@ -1,20 +1,7 @@
-import express from "express";
-import dotenv from "dotenv";
-import mainRoute from "./src/routes/main.js";
-import watchRoute from "./src/routes/watch.js";
-import animeRoute from "./src/routes/anime.js";
+// BIENVENIDO A LA API NO ODIFICAL DE MONOSCHINOS
 
-dotenv.config();
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use("/api", mainRoute);
-
-app.use("/api", watchRoute);
-
-app.use("/api", animeRoute);
-
-app.listen(PORT, () => {
-  console.log(`🚀 API corriendo en http://localhost:${PORT}`);
-});
+ENDPOINTS_DISPONIBLES = {
+  home: "/api/main",
+  watch: "/api/watch?url=",
+  animeInfo: "/api/anime?url=",
+};
