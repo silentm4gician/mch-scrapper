9e932b5ef8fb4a60bd14df680146ba2e683dc011
silentm4gician <leandrogonzalezmat@gmail.com>
2025-04-08 00:46:51 -0300

    cherio xd

--- api/watch.js
+++ api/watch.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import cheerio from "cheerio";
+import * as cheerio from "cheerio";
 import dotenv from "dotenv";
 import { getCache, setCache } from "../src/services/cacheService.js";
 
